a09fc7bd01d804e3e003ce90f9bdec5b7b9cfc57
Jarvis <13g7895123@gmail.com>
2022-11-06 12:22:12 +0800

    login page action

---
+++ config/config.py
@@ -0,0 +1,10 @@
+# 檔案路徑
+firefox_path = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+driver_path = './geckodriver.exe'
+
+# 網址
+web_url = 'https://clock.eng-astra.com.tw/clock-system.php'
+
+# 輸入資料
+email = 'astra235@3tsai.com'
+pwd = 'Astrawm867'
