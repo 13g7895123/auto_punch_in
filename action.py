dd30b6a6c53d4a4585363a7826467328d538495f
Jarvis <13g7895123@gmail.com>
2022-11-06 12:59:07 +0800

    devide action

---
+++ action.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from config.config import *
+
+# 爬蟲配置
+driver_service = Service(executable_path=driver_path)
+driver = webdriver.Firefox(firefox_binary=firefox_path, service=driver_service)
+
+# 開啟爬蟲
+driver.get(web_url)
+
+
+# 登入
+def login():
+    # 取得網頁欄位
+    input_email = driver.find_element(By.ID, 'inputEmail')
+    input_pwd = driver.find_element(By.ID, 'inputPassword')
+    login_btn = driver.find_element(By.XPATH, "//button[text()='登入']")
+
+    # 操作網頁
+    input_email.send_keys(email)
+    input_pwd.send_keys(pwd)
+    login_btn.click()
+
+
+# 打卡上班
+def punch_in():
+    punch_in_btn = driver.find_element(By.XPATH, "//button[text()='打卡上班']")
+    punch_in_btn.click()
+
+
+# 打卡下班
+def punch_out():
+    punch_out_btn = driver.find_element(By.XPATH, "//button[text()='打卡下班']")
+    punch_out_btn.click()
--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 # 檔案路徑
 firefox_path = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-driver_path = './geckodriver.exe'
+driver_path = r'D:\Python\Tools\geckodriver.exe'
 
 # 網址
 web_url = 'https://clock.eng-astra.com.tw/clock-system.php'
@@ -8,3 +8,5 @@ web_url = 'https://clock.eng-astra.com.tw/clock-system.php'
 # 輸入資料
 email = 'astra235@3tsai.com'
 pwd = 'Astrawm867'
+
+
