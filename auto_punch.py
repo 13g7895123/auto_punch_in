1f7664e89ccc35faed9c0db68abc92e7fa4beb5f
Jarvis <13g7895123@gmail.com>
2022-12-01 13:52:43 +0800

    清除DEBUG用PRINT

--- auto_punch.py
+++ auto_punch.py
@@ -9,10 +9,6 @@ today_weekday = datetime.datetime.now().strftime('%A')  # 取得星期幾
 
 if today_weekday != 'Saturday' and today_weekday != 'Sunday':   # 周末不執行
     if today not in except_date_list:                           # 例外日期不執行
-        # now_time = str(now_time)
-        # now_time = now_time[-4, 0]
-        print('現在時間: ' + now_time)
-        print('早上打卡區間: {}~{}'.format(punch_in_morning_start, punch_in_morning_end))
         if punch_in_morning_start <= now_time < punch_in_morning_end \
                 or punch_in_noon_start <= now_time < punch_in_noon_end:
             login()
